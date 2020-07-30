11622bd3fcf594ec68e78528884217233fb2ff59
ThomsonDem <66383557+ThomsonDem@users.noreply.github.com>
2020-07-30 14:46:57 +0200

    Update server seed.sh

--- server seed.sh
+++ server seed.sh
@@ -1,28 +1,3 @@
-#!/bin/sh
-echo "________________________________"
-echo "TorServerSeed"
-echo "________________________________"
-echo"install software"
-apt-get install tor -y
-apt-get install apache2 -y
-echo "________________________________"
-echo "configure"
-echo "________________________________"
-echo "HiddenServiceDir /var/lib/tor/hidden_service/http" >> /etc/tor/torrc 
-read -p "insert IP : " ip
-echo " HiddenServicePort 80 $ip" >> /etc/tor/torrc
-echo "________________________________"
-echo ""
-echo "|see the status on /home/status|"
-echo "________________________________"
-service apache2 status >> /home/statusapache.txt
-service tor status >> /home/statustor.txt
-echo "________________________________"
-echo "our dark link: " 
-echo ".............."
-cat /var/lib/tor/hidden_services/hostname
-echo ".............."
-echo "________________________________"
 
 
 
